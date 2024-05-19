18468a6e32e4941bd04a98c299f334d821a2e0e0
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-05-19 21:29:51 +0200

    136 - 141

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -26,9 +26,6 @@ export default function App() {
   );
 }
 
-console.log(<DifferentContent test={23} />);
-console.log(DifferentContent());
-
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -70,7 +67,22 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+  function tripleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(`Batching`);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -86,13 +98,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={tripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
         <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
