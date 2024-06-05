f90936a3c963d7c425237321e09a5af7fdb37d65
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-06-05 22:05:55 +0200

    193 - 198

---
+++ react-quiz/src/components/Progress.js
@@ -0,0 +1,20 @@
+function Progress({ index, numQuestion, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestion}
+        value={index + Number(answer != null)}
+      ></progress>
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestion}
+      </p>
+      <p>
+        <strong>
+          {points} / {maxPossiblePoints}
+        </strong>
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
