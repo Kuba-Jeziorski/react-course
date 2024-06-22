8ee92586efcafab626eea40e7b20c50713e28ffc
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-06-22 18:48:23 +0200

    223* - 226

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -12,6 +12,7 @@ import { useParams, useSearchParams } from "react-router-dom";
 
 function City() {
   const { id } = useParams();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
