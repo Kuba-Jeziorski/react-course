e8aa7219d8c70add6f6df50c9ab6736baa290bd0
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-11-17 18:12:34 +0000

    292 - 298

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,13 +1,13 @@
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 p-4 uppercase text-stone-200">
+      <p className="space-x-4 font-semibold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
       <a href="/cart">Open cart &rarr;</a>
     </div>
-  );
+  )
 }
 
-export default CartOverview;
+export default CartOverview
---
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold">Kuba</div>
+}
+
+export default Username
