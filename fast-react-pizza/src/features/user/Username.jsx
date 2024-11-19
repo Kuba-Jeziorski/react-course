a6f87a304ee4994472f1df3c92d79be7928e62ec
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-11-19 18:51:06 +0000

    299 - 300

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Kuba</div>
+  return <div className="hidden text-sm font-semibold md:block">Kuba</div>
 }
 
 export default Username
