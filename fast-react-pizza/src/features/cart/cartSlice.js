64280b2fc5e67cb8bf076c631fdcceee2b646d65
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-12-04 19:02:22 +0000

    319 - 320

--- fast-react-pizza/src/features/cart/cartSlice.js
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -36,6 +36,10 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload)
       item.quantity--
       item.totalPrice = item.quantity * item.unitPrice
+
+      if (item.quantity === 0) {
+        cartSlice.caseReducers.deleteItem(state, action)
+      }
     },
     clearCart(state) {
       state.cart = []
@@ -59,3 +63,5 @@ export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0
