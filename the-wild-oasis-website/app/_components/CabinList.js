6403109226f8b7bf243bdbdf12bfd9ed49b6c11c
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2025-09-27 20:17:11 +0200

    461 - 465

--- the-wild-oasis-website/app/_components/CabinList.js
+++ the-wild-oasis-website/app/_components/CabinList.js
@@ -2,7 +2,7 @@ import { unstable_noStore as noStore } from "next/cache";
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "./CabinCard";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
   // useful for partial pre-rendering
 
@@ -12,10 +12,27 @@ async function CabinList() {
     return null;
   }
 
+  let displayedCabins;
+  if (filter === "all") {
+    displayedCabins = cabins;
+  }
+  if (filter === "small") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  }
+  if (filter === "large") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.length > 0 &&
-        cabins.map((cabin) => <CabinCard cabin={cabin} key={cabin.id} />)}
+      {displayedCabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
     </div>
   );
 }
